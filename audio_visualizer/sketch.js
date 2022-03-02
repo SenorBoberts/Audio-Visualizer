6855bf0daad0cf0ca5c83c7f130282c740ee34fa
Benjamin Roberts <benjaminrobertsplus1@gmail.com>
2022-03-02 15:36:17 -0500

    Added song select functionality

--- audio_visualizer/sketch.js
+++ audio_visualizer/sketch.js
@@ -7,16 +7,13 @@ let toggle;
 
 //funcs
 function preload(){
-	song = loadSound('assets/skate.mp3');
+	song = loadSound($("#songSelect").val());
 }
 
 function setup(){
-	createCanvas(256,256);
+	createCanvas(1024,512);
 	angleMode(DEGREES);
-	//toggle = createButton('toggle');
-	//toggle.mousePressed(toggleSong);
-	song.play();
-	fft = new p5.FFT(0,256);
+	fft = new p5.FFT(0,512);
 }
 
 function draw(){
@@ -26,6 +23,7 @@ function draw(){
 		let tall = spect[i];
 		let y = map(tall, 0, 256, height, 0);
 		line(i, height, i, y);
+		line(1024-i, height, 1024-i, y);
 	}
 	stroke(255);
 	noFill();
